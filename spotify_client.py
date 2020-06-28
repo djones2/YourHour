6d4da2d68e32c600084ed9c8d4fba428f4a62c36
Daniel <dcjones2015@gmail.com>
2020-06-28 10:36:30 -0700

    Init

---
+++ spotify_client.py
@@ -0,0 +1,43 @@
+import random
+import requests
+import string
+import urllib
+
+
+class SpotifyClient(object):
+	def __init__(self, api_token):
+		self.api_token = api_token
+
+	def get_random_tracks(self):
+		wildcard = f'%{random.choice(string.ascii_lowercase)}%'
+		print(wildcard)
+		query = urllib.parse.quote(wildcard)
+		offset = random.randint(0, 2000)
+		url = f"https://api.spotify.com/v1/search?q={query}&offset={offset}&type=track"
+		response = requests.get(
+			url,
+			headers={
+					"Content-Type": "application/json",
+					"Authorization": f"Bearer {self.api_token}"
+			}
+		)
+		response_json = response.json()
+		#print(response_json)
+		tracks = [track for track in response_json['tracks']['items']]
+		print(f'Found {len(tracks)} tracks to add to your library')
+		return tracks
+
+	def add_tracks_to_library(self, track_ids):
+		url = "https://api.spotify.com/v1/1Evp0hH7GxgnbJFKRxjmgm/tracks"
+		response = requests.put(
+			url,
+			json={
+				"ids": track_ids
+			},
+			headers={
+				"Content-Type": "application/json",
+				"Authorization": f"Bearer {self.api_token}"
+			}
+		)
+		print(response)
+		return response.ok
